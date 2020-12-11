5958e82b81b59a5c8855e7838a2cd0d4a46bd605
MĂDĂLINA-MARIA PUI <madalinapuimp@gmail.com>
2020-12-11 14:25:05 +0200

    add

--- My Work/my-work.js
+++ My Work/my-work.js
@@ -40,6 +40,13 @@ displayAssignmentDiv("Introduction to HTML 1",
 displayAssignmentDiv("Introduction to HTML 2",
                      "Implement the template of the following picture with HTML & CSS only.",
                      "assignment2");
+displayAssignmentDiv("Semantic HTML",
+                     "Update one of the previous homework to use semantic elements and CSS positioning techniques that you learned about. Use in your code as many elements of the following: article, aside, section, header, footer, nav, main. Try to have at least one element that's positioned using float, relative, absolute and fixed position values. Use other CSS properties to style your content, keeping in mind to avoid repetition, and to keep them well organized.",
+                     "assignment3");
+displayAssignmentDiv("Layouting Practice 1",
+                     "Implement the following design using HTML and CSS. The font used in the layout is called \"Muli\". Use the PerfectPixel Chrome add-on to find out font sizes.",
+                     "assignment4");
+
 
 //hyperlinks:
 const backButton = document.getElementById("home-button");
@@ -67,49 +74,49 @@ function visitAboutMePage() {
 aboutMeButton.addEventListener("click", visitAboutMePage);
 
 //V2 it worked at first-
-// const assignment1 = [{
+// const assignments = [{
 //     h3Element: "Introduction to HTML 1",
 //     pElement: "Implement with HTML and CSS your personal CV.",
 //     divID: "assignment1" 
-// }];
-
-// const assignment2 = [{
+// }, {
 //     h3Element: "Introduction to HTML 2",
 //     pElement: "Implement the template of the following picture with HTML & CSS only.",
 //     divID: "assignment2"
 // }]
 
 // function displayAssignmentDiv(assignment1) {
-//     const assignmentDiv = document.createElement("div");
-//     assignmentDiv.setAttribute("id", assignment1.divID);
-//     assignmentDiv.classList.add("assignment");
+//     for (let i = 1; i <= assignments.length; i++) {
+//         const assignmentDiv = document.createElement("div");
+//         assignmentDiv.setAttribute("id", assignment1.divID);
+//         assignmentDiv.classList.add("assignment");
 
-//     const assignmentDetailsDiv = document.createElement("div");
+//         const assignmentDetailsDiv = document.createElement("div");
 
-//     const title = document.createElement("h3");
-//     const titleNode = document.createTextNode(assignment1.h3Element);
-//     title.classList.add("assignment-details-title");
+//         const title = document.createElement("h3");
+//         const titleNode = document.createTextNode(assignment[i].h3Element);
+//         title.classList.add("assignment-details-title");
 
-//     title.appendChild(titleNode);
-//     assignmentDetailsDiv.appendChild(title);
+//         title.appendChild(titleNode);
+//         assignmentDetailsDiv.appendChild(title);
 
-//     const requirements = document.createElement("p");
-//     const requirementsNode = document.createTextNode(assignment1.pElement);
+//         const requirements = document.createElement("p");
+//         const requirementsNode = document.createTextNode(assignment[i].pElement);
 
-//     requirements.appendChild(requirementsNode);
-//     assignmentDetailsDiv.appendChild(requirementsNode);
+//         requirements.appendChild(requirementsNode);
+//         assignmentDetailsDiv.appendChild(requirementsNode);
 
-//     assignmentDetailsDiv.classList.add("assignment-details");
+//         assignmentDetailsDiv.classList.add("assignment-details");
 
-//     assignmentDiv.appendChild(assignmentDetailsDiv);
+//         assignmentDiv.appendChild(assignmentDetailsDiv);
 
-//     const iframe = document.createElement("iframe");
-//     iframe.classList.add("iframes");
+//         const iframe = document.createElement("iframe");
+//         iframe.classList.add("iframes");
 
-//     assignmentDiv.appendChild(iframe);
+//         assignmentDiv.appendChild(iframe);
 
-//     container.appendChild(assignmentDiv);
+//         container.appendChild(assignmentDiv);
+//     }
 // }
 
-// displayAssignmentDiv(assignment1);
-// displayAssignmentDiv(assignment2);
+// displayAssignmentDiv(assignment[1]);
+// displayAssignmentDiv(assignment[2]);
